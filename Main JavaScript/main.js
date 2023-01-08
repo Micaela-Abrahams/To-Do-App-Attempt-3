dcc2e0370d9407830c402f24a67deada8ab06b9a
Micaela-Abrahams <109106058+Micaela-Abrahams@users.noreply.github.com>
2023-01-08 21:33:37 +0200

    commit

--- Main JavaScript/main.js
+++ Main JavaScript/main.js
@@ -5,10 +5,8 @@ window.addEventListener('load', () => {
 	const sortBtn = document.querySelector('#sortBtn');
 	const inputField = document.querySelector('#inputField'); //
 	
-
 	// To-Do items are sorted alphabetically after the session has been refreshed
 	tasks.sort((a, b) => a.content.localeCompare(b.content));
-
 
 	//Submit form when 'Add' button clicked
 	newTaskForm.addEventListener('submit', e => {
@@ -80,57 +78,58 @@ function displayTasks () {
 		displayTasks();
 		  });
 		
-		content.classList.add('taskContent');
-		date.classList.add('taskDate');
-		actions.classList.add('actions');
-		edit.classList.add('edit');
+			content.classList.add('taskContent');
+			date.classList.add('taskDate');
+			actions.classList.add('actions');
+			edit.classList.add('edit');
 
-		deleteButton.classList.add('delete');
+			deleteButton.classList.add('delete');
 		
-		content.innerHTML = `<input type="text" value="${task.content}">`;
-		date.innerHTML = `<input type="date" value="${task.date}" readonly>`;
-		edit.innerHTML = 'Edit';
-		deleteButton.innerHTML = 'Delete';
+			content.innerHTML = `<input type="text" value="${task.content}">`;
+			date.innerHTML = `<input type="date" value="${task.date}">`;
+			edit.innerHTML = 'Edit';
+			deleteButton.innerHTML = 'Delete';
 		
-		label.appendChild(input);
-		label.appendChild(span);
+			label.appendChild(input);
+			label.appendChild(span);
 		
-		actions.appendChild(edit);
+			actions.appendChild(edit);
 			
-		taskItem.appendChild(label);
-		taskItem.appendChild(content);
-		taskItem.appendChild(date);
-		taskItem.appendChild(actions);
+			taskItem.appendChild(label);
+			taskItem.appendChild(content);
+			taskItem.appendChild(date);
+			taskItem.appendChild(actions);
 
-		
-    	// Append the task item to the task list
-    	taskList.appendChild(taskItem);
+			// Append the task item to the task list
+    		taskList.appendChild(taskItem);
 	
-		// Event Listeners for the Edit & Save Buttons
-		edit.addEventListener('click', () => {
+			/*The following code block entails the following when the user clicks the 'Edit' button
+			1) When user clicks the edit button, the input field will clear the previous text submitted
+			2) In the now empty input field, user can update their task accordingly
+			3) Beside the input field, two buttons will appear 'Save' & 'Cancel'
+			4) When user clicks the 'Save' button, the updated/edited text that was input in the text field will be updated to the DOM
+			5) Should user click 'Cancel', the input field will remain empty*/
+
+			// Event Listeners for the Edit Button
+			edit.addEventListener('click', () => {
 			// Clear the contents of the input field
 			content.innerHTML = '';
-		  
-			// Create a new input field for editing the task
-			const editInput = document.createElement('input');
+		  	const editInput = document.createElement('input'); // Create a new input field for editing the task
 			editInput.type = 'text';
 
-			// Clear the text in the input field
-  			editInput.value = '';
+			editInput.value = ''; // Clear the text in the input field
   			content.appendChild(editInput);
 
-		  	// Set the focus on the input field when edit button is clicked
-			editInput.focus();
+		  	editInput.focus();// Set the focus on the input field when edit button is clicked
 		  
-			// Create a save & cancel button
-			const saveButton = document.createElement('button');
-			const cancelButton = document.createElement('button')
+			const saveButton = document.createElement('button'); // Create a save button
+			const cancelButton = document.createElement('button') // Create a cancel button
 
 			saveButton.innerHTML = 'Save';
 			cancelButton.innerHTML = 'Cancel';
 
 			saveButton.classList.add('save-button');//Class added to the save button
-			cancelButton.classList.add('cancel-button');
+			cancelButton.classList.add('cancel-button');//Class added to the cancel button
 
 			content.appendChild(saveButton);
 			content.append(cancelButton);
@@ -140,31 +139,21 @@ function displayTasks () {
 		  
 			// Add a click event listener to the save button
 			saveButton.addEventListener('click', () => {
-			  // Update the task's content
-			  task.content = editInput.value;
-		  
-			  // Update the task in local storage
-			  localStorage.setItem('tasks', JSON.stringify(tasks));
-
-
-			  // Re-render the task list
-			  displayTasks();
-
-			  // Show the edit button again after the save button is clicked
-    		 edit.style.display = 'inline-block';
+			task.content = editInput.value; // Update the task's content	  	
+		  	localStorage.setItem('tasks', JSON.stringify(tasks)); // Update the task in local storage
+		  	displayTasks();// Re-render the task list
+			edit.style.display = 'inline-block';//Display the Edit Button after the save button is clicked
 			});
 
 			//Add a click event listener to the cancel button
 			cancelButton.addEventListener('click', () =>{
-				 cancelButton.style.display = 'none'; //Hide Cancel Button
-				 saveButton.style.display = 'none'; //Hide Save Button
-				 edit.style.display = 'inline-block'; //Display Edit Button
+			cancelButton.style.display = 'none'; //Hide Cancel Button
+			saveButton.style.display = 'none'; //Hide Save Button
+			edit.style.display = 'inline-block'; //Display Edit Button
 			})
-
-
 		  });
 		  
-		//Functionality for 'Delete' Button
+			//Add a click event listener to the delete button
 			deleteButton.addEventListener('click', () => {
 			tasks.splice(tasks.indexOf(task), 1);
 			localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -181,29 +170,27 @@ function displayTasks () {
 		  } else {
 			taskList.appendChild(taskItem);
 		  }
-			});
-
-}
-
-//Listen for sort button being clicked
-sortBtn.addEventListener('click', (e,)=> {
-	//Sort task objects in tasks array alphabetically
-	tasks.sort((a, b) => {
-		let ca = a.content.toLowerCase(),
+		});
+	}
+			//Add a click event listener to the Sort button
+			sortBtn.addEventListener('click', (e,)=> {
+			//Sort task objects in tasks array alphabetically
+			tasks.sort((a, b) => {
+			let ca = a.content.toLowerCase(),
 			cb = b.content.toLowerCase();
 	
-		if (ca < cb) {
+			if (ca < cb) {
 			return -1;
-		}
-		if (ca > cb) {
+			}
+			if (ca > cb) {
 			return 1;
-		}
-		return 0;
-	});
+			}
+			return 0;
+		});
 
-	//Store re-ordered tasks to local storage
-	localStorage.setItem('tasks', JSON.stringify(tasks));		
-	displayTasks();
+			//Store re-ordered tasks to local storage
+			localStorage.setItem('tasks', JSON.stringify(tasks));		
+			displayTasks();
 
-})
+		})
 
